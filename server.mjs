89657145942322160f76c637b09d46549831882f
nidhig-dev <goyal.nidhi@gmail.com>
2025-09-03 18:22:34 -0400

    Added middle ware

--- server.mjs
+++ server.mjs
@@ -2,9 +2,15 @@ import express from "express";
 import globalErr from "./middleware/globalErrorHandle.mjs";
 import fs from "fs";
 import baseRoutes from "./routes/baseRoutes.mjs"
+import morgan from "morgan";
 
 const app=express();
 const PORT=3000;
+
+// middle ware
+
+app.use(morgan('combined'));
+
 //define template engine
 app.engine("perscholas",(path,options,callback)=>{
     fs.readFile(path,(error,content)=>{
@@ -14,7 +20,8 @@ app.engine("perscholas",(path,options,callback)=>{
         let rendered=content
         .toString()
         .replaceAll("#heading#",options.heading)
-        .replace("#info#",options.info);
+        .replace("#info#",options.info)
+        .replace("#title",options.title);
     return(callback(null,rendered)); 
 
     })
@@ -30,14 +37,16 @@ app.use("/",baseRoutes);
 app.get("/home",(req,res)=>{
     let option={
         heading:"This is my Cat heading page",
-        info:"My cat is the most obedient cat in the whole world"
+        info:"My cat is the most obedient cat in the whole world",
+        title:"My cat home Page"
     };
     res.render("home",option);
 })
 app.get("/form", (req, res) => {
     let option = {
         heading: "My cat form",
-        info: "Enter cat name"
+        info: "Enter cat name",
+        title:"Form page"
     };
     res.render("form", option);
 })
